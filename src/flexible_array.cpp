00cfec9c6f4e9e1242eab1af676732f8f2f9dfe1
schaefereric <schaefereric@pm.me>
2024-02-20 19:14:55 +0100

    - added comments in flexible_array - added gui layouts file for rguilayout

--- src/flexible_array.cpp
+++ src/flexible_array.cpp
@@ -3,31 +3,34 @@
 #include <iostream>
 #include "color.hpp"
 
-
-int flexible_array::getSingleIndex(unsigned int ix, unsigned int iy) const  // Takes two index numbers (x and y) and returns single array index number
+// Takes two index numbers (x and y) and returns single array index number
+int flexible_array::getSingleIndex(unsigned int ix, unsigned int iy) const  
 {
     return ix + size_x * iy;
 }
 
-flexible_array::flexible_array(unsigned int ix, unsigned int iy) { // Enter field size in parameters
+// Recommended constructor, enter field size in parameters
+flexible_array::flexible_array(unsigned int ix, unsigned int iy) {
     size_x = ix;
     size_y = iy;
     pAry = new int[ix * iy];
     clearArray();
 }
 
-flexible_array::flexible_array() { // Fallback constructor
+// Default constructor, initializes array as 2*2 matrix
+flexible_array::flexible_array() { 
     size_x = 2;
     size_y = 2;
     pAry = new int[2 * 2];
     clearArray();
 }
 
-
+// Destructor, deallocates memory on heap
 flexible_array::~flexible_array() {
     delete[] pAry;
 }
 
+// Resize array
 void flexible_array::reInitialize(unsigned int ix, unsigned int iy) {
     if (ix == 0 || iy == 0) {
         std::cerr << "flexible_array::reInitialize: ERROR: Size cannot be zero!" << "\n";
@@ -41,6 +44,7 @@ void flexible_array::reInitialize(unsigned int ix, unsigned int iy) {
     clearArray();
 }
 
+// Returns item at given index
 int flexible_array::getItem(unsigned int ix, unsigned int iy) const {
     if (ix >= size_x || iy >= size_y) {
         std::cerr << "flexible_array::getItem: ERROR: Invalid index!" << "\n";
@@ -50,6 +54,7 @@ int flexible_array::getItem(unsigned int ix, unsigned int iy) const {
     return pAry[getSingleIndex(ix, iy)];
 }
 
+// Sets items at given index
 void flexible_array::setItem(unsigned int ix, unsigned int iy, int input) {
     if (ix > size_x - 1 || iy > size_y - 1) {
         std::cerr << "flexible_array::setItem: ERROR: Invalid index!" << "\n";
@@ -59,14 +64,17 @@ void flexible_array::setItem(unsigned int ix, unsigned int iy, int input) {
     pAry[getSingleIndex(ix, iy)] = input;
 }
 
+// Returns array size_X
 unsigned int flexible_array::getSizeX() const {
     return size_x;
 }
 
+// Returns array size_Y
 unsigned int flexible_array::getSizeY() const {
     return size_y;
 }
 
+// Initialize array with zeros
 void flexible_array::clearArray() {
     for (unsigned int i = 0; i < size_x; i++) {
         for (unsigned int j = 0; j < size_y; j++) {
@@ -75,19 +83,26 @@ void flexible_array::clearArray() {
     }
 }
 
-void flexible_array::traverse() { // Prints a countinuous list of all items in the array
+// -----------------------------------------------------------
+// 
+// DEPRECATED DEBUG FUNCTIONS !!! ONLY WORKS WITH INTEGERS !!!
+//
+// 
+// Prints a countinuous list of all items in the array
+void flexible_array::traverse() { 
     for (unsigned int i = 0; i < size_x; i++) {
         for (unsigned int j = 0; j < size_y; j++) {
             std::cout << "X: " << i
-                << " Y: " << j
-                << " Item: " << getItem(i, j)
-                << "\n";
+                      << " Y: " << j
+                      << " Item: " << getItem(i, j)
+                      << "\n";
         }
     }
     std::cout << std::endl;
 }
 
-void flexible_array::printMatrix() { // Assuming the matrix is storing bools, a square matrix is printed, displaying zeros/false as O and ones/true as X
+// Assuming the array is storing bools, a square matrix is printed, displaying zeros/false as O and ones/true as X
+void flexible_array::printMatrix() { 
     for (unsigned int i = 0; i < size_x; i++) {
         for (unsigned int j = 0; j < size_y; j++) {
             if (getItem(i, j) == 0) {
--- src/flexible_array.hpp
+++ src/flexible_array.hpp
@@ -6,28 +6,45 @@ struct flexible_array {
 
     int* pAry; // Pointer to array on heap
 
-    int getSingleIndex(unsigned int ix, unsigned int iy) const;  // Takes two index numbers (x and y) and returns single array index number
+    // Takes two index numbers (x and y) and returns single array index number
+    int getSingleIndex(unsigned int ix, unsigned int iy) const; 
 
+    // Recommended constructor, enter field size in parameters
     flexible_array(unsigned int ix, unsigned int iy);
 
+    // Default constructor, initializes array as 2*2 matrix
     flexible_array();
 
+    // Destructor, deallocates memory on heap
     ~flexible_array();
 
+    // Resize array
     void reInitialize(unsigned int ix, unsigned int iy);
 
+    // Returns item at given index
     int getItem(unsigned int ix, unsigned int iy) const;
 
+    // Sets items at given index
     void setItem(unsigned int ix, unsigned int iy, int input);
 
+    // Returns array size_X
     unsigned int getSizeX() const;
 
+    // Returns array size_Y
     unsigned int getSizeY() const;
 
+    // Initialize array with zeros
     void clearArray();
 
+    // -----------------------------------------------------------
+    // 
+    // DEPRECATED DEBUG FUNCTIONS !!! ONLY WORKS WITH INTEGERS !!!
+    //
+    // 
+    // Prints a countinuous list of all items in the array
     void traverse();
 
+    // Assuming the array is storing bools, a square matrix is printed, displaying zeros/false as O and ones/true as X
     void printMatrix();
 
 };
